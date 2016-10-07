ef0da2c3b47ad04dab5bff08e58fe839c938653a
smoleniuch <jan_13@o2.pl>
2016-10-07 10:58:47 +0200

    Bootstrap,jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width ,initial-scale=1.0">
+  <title>Firtness tracker</title>
+</head>
+	
+<body>
+
+  <script src="jquery/jquery-3.1.1.js"></script>
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</body>
+	
+</html>
